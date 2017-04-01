66ac2da7a7a7d23bdb46a824da0cf90c3b4f29b1
Ben Lewis <benjf5+github@gmail.com>
2017-04-01 00:00:17 -0700

    Added RouteDetails page and codebehind, which were missing in my last commit.

---
+++ OneBusAway/RouteDetails.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using OneBusAway.Model.BusServiceDataStructures;
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace OneBusAway.View
+{
+  /// <summary>
+  /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+  /// </summary>
+  public sealed partial class RouteDetails : Page
+  {
+    #region Properties
+    public Route CurrentRoute { get; private set; }
+    #endregion
+    public RouteDetails()
+    {
+      this.InitializeComponent();
+    }
+
+    protected async override void OnNavigatedTo(NavigationEventArgs args)
+    {
+      CurrentRoute = (args.Parameter as Route);
+
+    }
+  }
+}
